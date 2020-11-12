869b31e0f9542a3c6adce87e65cd1dc0f22361f8
jensholzberger <jensholzberger@users.noreply.github.com>
2020-11-12 10:06:11 +0100

    daily update

--- ios/main.py
+++ ios/main.py
@@ -2,18 +2,42 @@ import ui
 import requests
 import json
 
+BACKUP_LIST = '''{
+    "modules": {
+        "peewee": [
+            "coleifer/peewee",
+            ["peewee.py"]
+        ]
+    },
+    "programms": {
+        "chat-app": ["Speziwelt",[['desktop/main.py'],['ios/main.py']]],
+        "musik": "Musikdatenbank"
+    }
+}'''
+
 
 class View(ui.View):
 	def did_load(self):
 		r = requests.get('https://raw.githubusercontent.com/jensholzberger/installer/master/apps.json')
-		liste =json.loads(r.content.decode().strip('\n'))
-		self.modules=liste['modules']
-		self.programms=liste['programms']
-		print([i for i in self.programms.values()])
+		liste = json.loads(r.content.decode().strip('\n'))
+		self.modules = liste['modules']
+		self.programms = liste['programms']
+		list1 = [i[0] for i in self.programms.values()]
+		self['tableview1'].data_source = ui.ListDataSource(items=list1)
+
+		self['segmentedcontrol1'].action = self.select_category
 		self.present('sheet')
 
 	def select_category(self, sender):
-		pass
+		if sender.selected_index:
+			liste = [i for i in self.modules.keys()]
+			self['tableview1'].data_source = ui.ListDataSource(items=liste)
+			self['tableview1'].reload()
+		else:
+			list1 = [i[0] for i in self.programms.values()]
+			self['tableview1'].data_source = ui.ListDataSource(items=list1)
+			self['tableview1'].reload()
 
 
 v = ui.load_view()
+
